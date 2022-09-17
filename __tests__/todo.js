9337ed50c9c280704d9d1641ced03b42e63d0cb3
mehulpatel510 <mspatel.it@spcevng.ac.in>
2022-09-17 23:45:53 +0530

    added test cases for overdue, duetoday and duelater

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,6 +1,6 @@
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater, } = todoList();
 
 describe("Todolist Test Suite", () => {
   beforeAll(() => {
@@ -23,5 +23,32 @@ describe("Todolist Test Suite", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+  test("Should retrieve a overdue items",() => {
+    const overdueItemsCount = overdue().length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: (new Date(new Date().setDate((new Date()).getDate() - 1))).toLocaleDateString("en-CA"),
+    });
+    expect(overdue().length).toBe(overdueItemsCount + 1);
+  });
+  test("Should retrieve a due today items",() => {
+    const dueTodayItemsCount = dueToday().length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(dueToday().length).toBe(dueTodayItemsCount + 1);
+  });
+  test("Should retrieve a due later items",() => {
+    const dueLaterItemsCount = dueLater().length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: (new Date(new Date().setDate((new Date()).getDate() + 1))).toLocaleDateString("en-CA"),
+    });
+    expect(dueLater().length).toBe(dueLaterItemsCount + 1);
   });
 });
--- todo.js
+++ todo.js
@@ -57,4 +57,5 @@ const todoList = () => {
   };
 };
 
+
 module.exports = todoList;
